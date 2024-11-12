86698cb5824a110ae131cb3c6890e38eb7ae07b4
soulninja-dev <68557696+soulninja-dev@users.noreply.github.com>
2024-11-11 23:36:37 -0600

    init, new screen and quits with q

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+use ratatui::{backend::CrosstermBackend, Terminal};
+
+use std::io;
+
+fn main() -> Result<(), io::Error> {
+    // setting up terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // main loop
+    loop {
+        terminal.draw(|_f| {})?;
+
+        // handle input events
+        if event::poll(std::time::Duration::from_millis(100))? {
+            if let Event::Key(key) = event::read()? {
+                if key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
+}
